516bb2acbbb6ea3bbe630f8d3b08af7b6961a626
fy-coder <76418928+fy-coder@users.noreply.github.com>
2022-02-15 20:24:55 +0800

    Update Login.vue

--- Login.vue
+++ Login.vue
@@ -1,114 +1,102 @@
 <template>
 	<view class="screen">
 		
-		<form @submit="formSubmit">
-			<input class="input" name="username" placeholder="请输入用户名" />
+		<!-- <uni-forms @submit="formSubmit">
+			<uni-easyinput class="input" name="username" placeholder="请输入用户名" />
 			<input class="input" name="password" placeholder="请输入密码" password=true/>
+			<uni-easyinput class="input" name="password" type="password" placeholder="请输入密码"></uni-easyinput>
 			<button class="button" form-type="submit" >登录</button>
-		</form>
+		</uni-forms> -->
+		
+		
+		
+		<uni-forms ref="form" :modelValue="formData">
+			<uni-forms-item name="username">
+				<uni-easyinput  v-model="formData.username" placeholder="请输入用户名" />
+			</uni-forms-item>
+			<uni-forms-item name="password">
+				<uni-easyinput  v-model="formData.password" type="password" placeholder="请输入密码"/>
+			</uni-forms-item>
+		</uni-forms>
+		<button class="button" @click="submit">登录</button>
 		<navigator class="button"  url="../Register/Register"><button>注册</button></navigator>
 		
-		<view class="uni-form-item uni-column">
-		    <view class="title"><text class="uni-form-item__title">可查看密码的输入框</text></view>
-		    <view class="uni-input-wrapper">
-		        <input class="uni-input" placeholder="请输入密码" :password="showPassword" />
-		        <text class="uni-icon" :class="[!showPassword ? 'uni-eye-active' : '']" @click="changePassword"> &#xe568; </text>
-		    </view>
-		</view>
+		
+		
 	</view>
 </template>
 
 <script>
 	export default {
-		onLoad() {
-			
-		},
 		data() {
 			return {
-				token:'',
-				isSuccess:false,
-				latitude:0,
-				longitude:0,
-				showPassword: true,
+				formData:{
+					username:'',
+					password:'',
+				},
 			}
 		},
-		onReady() {
-			uni.getStorage({
-				key:'userInfo',
-				success:function(res)
-				{
-					uni.showToast({
-						title:'您已登录',
-					})
-				}
-			})
-		},
 		methods: {
-			changePassword: function() {
-			    this.showPassword = !this.showPassword;
-			},
-			formSubmit:function(e){
-				console.log("formSubmit");
-				
-				uni.request({
-					url:'http://47.97.90.35:8080/login',
-					method:'POST',
-					data:JSON.stringify(e.detail.value),
-					success: res => {
-						if(res.data.code=="200"){
+			submit:function(){
+				this.$refs.form.validate().then(res=>{
+					// console.log(res);
+					// console.log(JSON.stringify(res));
+					uni.request({
+						url:'http://47.97.90.35:8080/login',
+						method:'POST',
+						data:JSON.stringify(res),
+						success: r => {
+							console.log(r);
+							if(r.data.code=="200"){
+								uni.showToast({
+									title: '登录成功'
+								});
+								uni.setStorage({
+									key:'username',
+									data:JSON.stringify(res.username),
+									success(data) {
+										console.log('设置username完成')
+										//console.log(data)
+									}
+								})
+								uni.setStorage({
+									key:'password',
+									data:JSON.stringify(res.password),
+									success(data) {
+										console.log('设置password完成')
+										//console.log(data)
+									}
+								})
+								uni.setStorage({
+									key:'token',
+									data:JSON.stringify(r.data.data.token),
+									complete(data) {
+										console.log('设置token完成')
+										//console.log(data)
+										console.log('即将跳转');
+										uni.switchTab({//执行顺序有些问题
+											url:'/pages/Map/Map',
+										})
+									}
+								})
+							}
+							else{
+								uni.showToast({
+									title: '请检查用户名或密码',
+									icon:"none"
+								});
+							}
+						},
+						fail() {
 							uni.showToast({
-								title: '登录成功'
-							});
-							this.token=res.data.token;
-							uni.setStorage({
-								key:'userInfo',
-								data:JSON.stringify(e.detail.value.username),
-								complete(data) {
-									console.log('设置userInfo完成')
-									console.log(data)
-								}
+								title:"网络错误，请检查网络"
 							})
-							uni.setStorage({
-								key:'token',
-								data:JSON.stringify(res.data.data.token),
-								complete(data) {
-									console.log('设置token完成')
-									console.log(data)
-									console.log('即将跳转');
-									uni.switchTab({//执行顺序有些问题
-										url:'/pages/Home/Home',
-									})
-								}
-							})
-							this.isSuccess=true;
-							
-							// uni.getLocation({
-							// 	success(res) {
-							// 		uni.redirectTo({
-							// 			url:'../Map/Map?data='+JSON.stringify({latitude:res.latitude,longitude:res.longitude}) ,
-							// 			complete() {
-							// 				console.log(res.latitude)
-							// 				console.log(res.longitude)
-							// 			}
-							// 		})
-								
-							// 	}
-							// })	
 						}
-						else{
-							uni.showToast({
-								title: '请检查用户名或密码',
-								icon:"none"
-							});
-						}
-					},
-					fail() {
-						uni.showToast({
-							title:"网络错误，请检查网络"
-						})
-					}
+					})
+				}).catch(err=>{
+					console.log('no');
+					console.log(err);
 				})
-				
 			},
 		}
 	}
